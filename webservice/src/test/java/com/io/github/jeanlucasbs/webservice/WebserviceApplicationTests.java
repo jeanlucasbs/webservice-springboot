bd7d6dc447e60c36712e7007a0335aebcc89ef7d
jeanlucasbs <jean.silva@discente.ufg.br>
2023-06-29 00:03:06 -0300

    Project Created

---
+++ webservice/src/test/java/com/io/github/jeanlucasbs/webservice/WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.jeanlucasbs.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
