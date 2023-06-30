134f4f67916d170b7550ca08feb9f9c093bf9179
jeanlucasbs <jean.silva@discente.ufg.br>
2023-06-30 00:42:10 -0300

    Implementation class Order

--- webservice/src/main/java/com/io/github/jeanlucasbs/webservice/entities/User.java
+++ webservice/src/main/java/com/io/github/jeanlucasbs/webservice/entities/User.java
@@ -1,12 +1,15 @@
 package com.io.github.jeanlucasbs.webservice.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,18 +24,21 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {}
 	
-	public User(Long iD, String name, String email, String phone, String password) {
+	public User(Long iD, String name, String email, String phone, String password/*, List<Order> orders*/) {
 		super();
 		ID = iD;
 		this.name = name;
 		this.email = email;
 		this.phone = phone;
 		this.password = password;
+		//this.orders = orders;
 	}
-	
 
 	public Long getID() {
 		return ID;
@@ -77,6 +83,11 @@ public class User implements Serializable{
 	@Override
 	public int hashCode() {
 		return Objects.hash(ID);
+	}
+	
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
