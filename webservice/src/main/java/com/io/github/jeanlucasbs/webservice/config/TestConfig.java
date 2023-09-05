ee93fb1262fcd23cfd4d5002de34fbed24cd3427
jeanlucasbs <jean.silva@discente.ufg.br>
2023-09-04 23:35:41 -0300

    Novos funcionalidades

--- webservice/src/main/java/com/io/github/jeanlucasbs/webservice/config/TestConfig.java
+++ webservice/src/main/java/com/io/github/jeanlucasbs/webservice/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.io.github.jeanlucasbs.webservice.entities.Category;
 import com.io.github.jeanlucasbs.webservice.entities.Order;
+import com.io.github.jeanlucasbs.webservice.entities.OrderItem;
 import com.io.github.jeanlucasbs.webservice.entities.Product;
 import com.io.github.jeanlucasbs.webservice.entities.User;
 import com.io.github.jeanlucasbs.webservice.entities.enums.OrderStatus;
 import com.io.github.jeanlucasbs.webservice.repositories.CategoryRepository;
+import com.io.github.jeanlucasbs.webservice.repositories.OrderItemRepository;
 import com.io.github.jeanlucasbs.webservice.repositories.OrderRepository;
 import com.io.github.jeanlucasbs.webservice.repositories.ProductRepository;
 import com.io.github.jeanlucasbs.webservice.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	ProductRepository productRepository;
+	
+	@Autowired
+	OrderItemRepository orderItemRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -66,6 +71,13 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p4.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p1.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 		}
 
